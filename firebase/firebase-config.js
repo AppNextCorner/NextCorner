9581059ca25604646f244e4307f45e0383ccf147
RealHenryMartinez <henrymtz0390@gmail.com>
2023-03-07 00:35:30 -0800

    fixing database data collection

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -2,3 +2,5 @@
  * Purpose of file: Store our firebase configuration for authentication and firebase app for frontend purposes - logging in 
  */
 
+
+// ignore this file because it is not used in firebase config -> refer to ./util/firebase.js
