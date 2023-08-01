e80c6cd0a1c16ea42798274b6066cce03491189f
RealHenryMartinez <henrymtz0390@gmail.com>
2023-08-01 16:28:40 -0700

    Developers | Firebase auth bug fi

--- hooks/handleUsers/useFirebase.js
+++ hooks/handleUsers/useFirebase.js
@@ -4,4 +4,16 @@ import { firebaseConfig } from "util/firebase.util";
 // firebase configuration / initialize
 const app = initializeApp(firebaseConfig);
 
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import {
+  getReactNativePersistence,
+  initializeAuth,
+} from "firebase/auth/react-native";
+
+// Use expo's async storage mechanism instead of the default firebase storage mechanism 
+initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
 export const auth = getAuth(app);
